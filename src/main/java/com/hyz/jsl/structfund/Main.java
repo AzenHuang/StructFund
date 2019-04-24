d5424c36aaf1258c898793a33ae71b74070299ec
huangyongzheng <huangyongzheng@meituan.com>
2019-04-24 13:05:30 +0800

    bugfix

--- src/main/java/com/hyz/jsl/structfund/Main.java
+++ src/main/java/com/hyz/jsl/structfund/Main.java
@@ -9,18 +9,21 @@ import java.util.*;
 
 public class Main {
     public static final String COMMA = ",";
-    public static final int MIN_VOLUME = 40;
+    public static final int MIN_A_VOLUME = 3;
+    public static final int MIN_VOLUME = 30;
     public static final float MIN_PROFIT_RATE = 0.015F;
     private static Map<String, AFund> aFundMap;
     private static Map<String, BFund> bFundMap;
     private static Set<String> applyFailedMFunds = new HashSet<>();
     private static Map<String,Integer> mFundsMinApplyMap = new HashMap<>();
+    private static Map<String,Integer> mFundsConfirmMap = new HashMap<>();
     private static final int DEFAULT_MIN_APPLY_VALUE = 1000;
     private static final int DEFAULT_MIN_CONFIRM_VALUE = 1000;
 
     static {
         //5W申购失败
         applyFailedMFunds.add("163113");//申万证券
+        applyFailedMFunds.add("502013");//长盛一带一路
         //1K申购失败
         applyFailedMFunds.add("165515");//信诚300
         applyFailedMFunds.add("502053");//长盛券商分级
@@ -32,7 +35,8 @@ public class Main {
     }
 
     static {
-        mFundsMinApplyMap.put("502010", 50000);//证券分级，申5W给1K
+        mFundsMinApplyMap.put("502010", 50000);//易方达证券分级，申5W给1K
+        mFundsMinApplyMap.put("502003", 50000);//易方达军工分级，申5W给1K
         mFundsMinApplyMap.put("160221", 50000);//国泰有色，限额5W
 //        mFundsMinApplyMap.put("160630", ？？？);//鹏华国防分级
         mFundsMinApplyMap.put("161024", 1000);//军工分级
@@ -40,6 +44,12 @@ public class Main {
         mFundsMinApplyMap.put("161027", 1000);//证券分级
         mFundsMinApplyMap.put("163109", 1000);//申万深成
 
+    }
+
+    static {
+        mFundsConfirmMap.put("502010", 1000);//易方达证券分级，申5W给1K
+        mFundsConfirmMap.put("502003", 50000);//易方达军工分级，申5W给1K
+        mFundsConfirmMap.put("160221", 50000);//国泰有色，限额5W
     }
 
     public static void main(String[] args) throws IOException {
@@ -81,26 +91,36 @@ public class Main {
             float aFundVolume = Float.parseFloat(motherFund.aFund.cell.fundaVolume);
             float bFundVolume = Float.parseFloat(motherFund.bFund.cell.fundbVolume);
 
-            if (aFundVolume > MIN_VOLUME && bFundVolume > MIN_VOLUME) {
+            float applyFee = 0;
+            try {
+                applyFee = percentFormat.parse(motherFund.cell.applyFee).floatValue();
+            } catch (ParseException e) {
+                //ignore
+            }
+            Integer minApply = mFundsMinApplyMap.containsKey(motherFund.cell.baseFundId) ? mFundsMinApplyMap.get(motherFund.cell.baseFundId) : DEFAULT_MIN_APPLY_VALUE;
+            float confirmValue = mFundsConfirmMap.containsKey(motherFund.cell.baseFundId) ? mFundsConfirmMap.get(motherFund.cell.baseFundId) : DEFAULT_MIN_CONFIRM_VALUE;
+            float capitalCost = (minApply / 10000F * 2F) / confirmValue;
+//                float safesPremiumRate = MIN_PROFIT_RATE + applyFee;
+            motherFund.expectedProfitRate = motherFund.splitPremiumRate - applyFee - capitalCost;
+
+            if (aFundVolume > MIN_A_VOLUME && bFundVolume > MIN_VOLUME) {
                 volumedMotherFundList.add(motherFund);
 
-                float applyFee = 0;
-                try {
-                    applyFee = percentFormat.parse(motherFund.cell.applyFee).floatValue();
-                } catch (ParseException e) {
-                    //ignore
-                }
-                Integer minApply = mFundsMinApplyMap.containsKey(motherFund.cell.baseFundId) ? mFundsMinApplyMap.get(motherFund.cell.baseFundId) : DEFAULT_MIN_APPLY_VALUE;
-                float capitalCost = (minApply / 10000F * 2F) / DEFAULT_MIN_CONFIRM_VALUE;
-//                float safesPremiumRate = MIN_PROFIT_RATE + applyFee;
-                motherFund.expectedProfitRate = motherFund.splitPremiumRate - applyFee - capitalCost;
                 if(!applyFailedMFunds.contains(motherFund.cell.baseFundId) && motherFund.expectedProfitRate > MIN_PROFIT_RATE){
                     targetMotherFundList.add(motherFund);
                 }
             }
 
         }
-        String limitDesc = "-成交" + MIN_VOLUME + "-收益率阈值" + MIN_PROFIT_RATE +"-";
+
+        Collections.sort(motherFundList);
+        File allFile = new File("data/StructFund-all-"
+                + new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())
+                + ".csv");
+        saveCsvFile(motherFundList,allFile);
+
+        String limitDesc = "-成交" + MIN_VOLUME+"-";
+        String minRateDesc = "收益率阈值" + MIN_PROFIT_RATE +"-";
         Collections.sort(volumedMotherFundList);
         File volumedFile = new File("data/StructFund-volumed-"+limitDesc
                 + new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())
@@ -108,7 +128,7 @@ public class Main {
         saveCsvFile(volumedMotherFundList,volumedFile);
 
         Collections.sort(targetMotherFundList);
-        File targetFile = new File("data/StructFund-target-"+limitDesc
+        File targetFile = new File("data/StructFund-target-"+limitDesc+minRateDesc
                 + new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())
                 + ".csv");
         saveCsvFile(targetMotherFundList,targetFile);
